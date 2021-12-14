8a8b109edcdd0e0c233123e7c80c23da66204d76
Victoria Khilko <vic.khilko@mail.ru>
2021-12-14 15:14:24 +0300

    [Change] delete outer directory [Add] rest

--- shop/api/categories/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-
-from .serializers import ProductSerializer
-from shop.categories.models import Product
-
-
-class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
--- shop/categories/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.categories_list, name='categories_list'),
-    path('<int:id>/', views.products_list, name='products_list'),
-    path('products/<int:id>/', views.product_description, name='product_description')
-]
